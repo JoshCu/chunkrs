f491d03c13c712ba5ed890df558d14f8c93a7a07
josh <josh.cu@gmail.com>
2025-05-01 02:13:32 -0500

    adds decompression

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 use pyo3::prelude::*;
 
 mod cli;
+mod decompress;
 mod downloader;
 mod utils;
 
 #[pymodule]
 fn dlpy(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(decompress::decompress_tarball, m)?)?;
     m.add_function(wrap_pyfunction!(downloader::download, m)?)?;
     m.add_function(wrap_pyfunction!(cli::cli_main, m)?)?;
     Ok(())
